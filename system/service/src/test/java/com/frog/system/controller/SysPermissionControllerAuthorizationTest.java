45ca32b3e189e43dd32508cc82da9811a5011679
厚礼蟹 <16126806+dengxingzhi@user.noreply.gitee.com>
2026-09-20 12:06:48 +0800

    test(system.service): SysPermissionController 测试排除 Dubbo 误触发 (@WebMvcTest 切片修复)

--- system/service/src/test/java/com/frog/system/controller/SysPermissionControllerAuthorizationTest.java
+++ system/service/src/test/java/com/frog/system/controller/SysPermissionControllerAuthorizationTest.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import com.frog.system.service.Impl.SysRegisteredClientServiceImpl;
 import com.frog.system.service.ISysPermissionService;
 import com.frog.system.service.ISysUserService;
 
@@ -23,7 +22,6 @@ class SysPermissionControllerAuthorizationTest {
     @Autowired MockMvc mvc;
     @MockitoBean ISysPermissionService permissionService;
     @MockitoBean ISysUserService sysUserService;
-    @MockitoBean SysRegisteredClientServiceImpl sysRegisteredClientService;
 
     @Test
     @WithMockUser(authorities = {"system:user:list"})
